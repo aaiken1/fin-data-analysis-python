5451a2b2cb988721921686717760af5806e52506
Adam Aiken <adam.aiken@gmail.com>
2022-02-07 09:38:53 -0500

    lab 1

---
+++ code/0204-class-work.py
@@ -0,0 +1,26 @@
+a = 10
+a
+type(a)
+
+42 == 42
+
+42 != 42
+
+43 > 42
+
+x = 23
+X = 42
+
+x == X
+
+# Define our string. Check the Jupyter:Variables in your VS Code! Note the size.
+txt = 'elon university'
+print(txt)
+
+
+
+
+
+
+
+
